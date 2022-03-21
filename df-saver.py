dc33ca426f5fa0882f466208f9f850c57ba584ce
gosha <george.kontsevik@gmail.com>
2022-03-21 12:19:52 +0300

    теперь к серверу подключается

--- df-saver.py
+++ df-saver.py
@@ -256,12 +256,12 @@ class Parser:
 
         # Group_1 "Connection to DB"
         parser_conn = parser.add_argument_group(title='Connection options')
-        # parser_conn.add_argument('-db-addr', nargs='?', const=0, default='10.32.1.62', type=str.lower)
-        parser_conn.add_argument('-db-addr', nargs='?', const=0, default='127.0.0.1', type=str.lower)
+        parser_conn.add_argument('-db-addr', nargs='?', const=0, default='10.32.1.62', type=str.lower)
+        # parser_conn.add_argument('-db-addr', nargs='?', const=0, default='127.0.0.1', type=str.lower)
         parser_conn.add_argument('-db-port', nargs='?', const=0, default=5432, type=int)
         parser_conn.add_argument('-db-name', nargs='?', const=0, default='city_db_final', type=str.lower)
-        # parser_conn.add_argument('-db-user', nargs='?', const=0, default='postgres', type=str.lower)
-        parser_conn.add_argument('-db-user', nargs='?', const=0, default='gk', type=str.lower)
+        parser_conn.add_argument('-db-user', nargs='?', const=0, default='postgres', type=str.lower)
+        # parser_conn.add_argument('-db-user', nargs='?', const=0, default='gk', type=str.lower)
         parser_conn.add_argument('-db-pass', nargs='?', const=0, default='postgres', type=str.lower)
 
         # Group_2 "Query data from DB"
